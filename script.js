ccf3c422bef97eba6485d6985af6fbe54eae6e37
Christianmagni97 <christianmagni28@gmail.com>
2024-05-21 15:47:36 +0200

    corretto scrittura all'interno del ciclo for

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let userSeconds = Number.parseInt(30);
 for (let index = 0; index < 5; index++) {
     const pcNumber = Math.floor (Math. random() * 100) + 1;
     console.log(pcNumber);
+    document.querySelector('h3').innerHTML + pcNumber;
+    
+
 }
 const timer = setInterval( function(){
     userSeconds--;  
     if( userSeconds <= 0){
         clearInterval(timer); 
-        alert('Finito il tempo !!!');
+        prompt('Quali numeri sono usciti?');
     }
 }, 1000);
